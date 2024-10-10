7d2ecf3e7748d44e14b5523889d4fa45be9169ca
Nikhilesh <nikhileshreddy861@gmail.com>
2024-10-10 22:56:08 +0530

    Adding a new file for next assignment purpose script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+let age = 21
+let hasstudent_ID = true
+let hasgovt_ID = false
+
+if(age>=18)
+{
+  if(hasstudent_ID== true ){
+     console.log("Membership Granted ")
+  }
+  else if(hasgovt_ID== true ){
+  console.log("Membership Granted")
+  }
+ else
+  {
+    console.log("Membership Denied - No valid ID")
+  }
+  
+}
+else {
+  console.log("Membership Denied - underage");
+}
+
+
+let age2 = 23
+let hasstudent_ID2 = false
+let hasgovt_ID2 = false
+
+if(age2>=18)
+{
+  if(hasstudent_ID2== true ){
+     console.log("Membership Granted ")
+  }
+  else if(hasgovt_ID2== true){
+  console.log("Membership Granted")
+  }
+ else
+  {
+    console.log("Membership Denied - No valid ID")
+  }
+  
+}
+else {
+  console.log("Membership Denied - underage");
+}
+
+let age3 = 17
+let hasstudent_ID3 = true
+let hasgovt_ID3 = false
+
+if(age3>=18)
+{
+  if(hasstudent_ID3== true ){
+     console.log("Membership Granted ")
+  }
+  else if(hasgovt_ID3== true ){
+  console.log("Membership Granted")
+  }
+ else
+  {
+    console.log("Membership Denied - No valid ID")
+  }
+  
+}
+else {
+  console.log("Membership Denied - underage");
+}
